1aa3e1171a462eb39a3768c631bddd88ca35c8d3
JunkyByte <adriano.donninelli@hotmail.it>
2023-04-28 23:05:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="iris",
-    version="0.0.1",
+    version="0.0.2",
     author="Adriano Donninelli",
     author_email="adriano.donninelli@hotmail.it",
     description="A command line tool to sync folders and files between local and remote paths",
